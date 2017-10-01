6db5254677ebf864e9f87be5b2bb13db1d30b246
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2017-10-01 22:47:18 +0200

    remove end '{' and not delete the last char of the output sentence

--- src/senser/AircraftSentenceFactory.java
+++ src/senser/AircraftSentenceFactory.java
@@ -2,17 +2,18 @@ package senser;
 
 import java.util.ArrayList;
 
-public class AircraftSentenceFactory
-{
-	public ArrayList<AircraftSentence> fromAircraftJson(String jsonAircraftList)
-	{	
-		//TODO: Get distinct aircrafts from the jsonAircraftList string
+public class AircraftSentenceFactory {
+	public ArrayList<AircraftSentence> fromAircraftJson(String jsonAircraftList) {
+		// TODO: Get distinct aircrafts from the jsonAircraftList string
 		// and store them in an ArrayList
 		ArrayList<AircraftSentence> aircraftList = new ArrayList<AircraftSentence>();
-		
+
+		// remove end '}'
+		jsonAircraftList = jsonAircraftList.substring(0, jsonAircraftList.length() - 1);
+
 		String[] seperated = jsonAircraftList.split("},");
-		for(String p: seperated){
-			p = p.substring(1, p.length() - 1);
+		for (String p : seperated) {
+			p = p.substring(1, p.length());
 			aircraftList.add(new AircraftSentence(p));
 		}
 		return aircraftList;
