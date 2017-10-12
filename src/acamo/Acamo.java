1a195d947b258c69a6a8b1f5b595b297b1a0159e
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2017-10-12 02:15:29 +0200

    add mouse event to table

--- src/acamo/Acamo.java
+++ src/acamo/Acamo.java
@@ -1,6 +1,5 @@
 package acamo;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Observable;
@@ -27,47 +26,46 @@ import messer.BasicAircraft;
 import messer.*;
 import senser.Senser;
 
-public class Acamo extends Application implements Observer
-{
+public class Acamo extends Application implements Observer {
 	private ActiveAircrafts activeAircrafts;
-    private TableView<BasicAircraft> table = new TableView<BasicAircraft>();
-    private ObservableList<BasicAircraft> aircraftList = FXCollections.observableArrayList();
-    private ArrayList<String> fields;
-    private ArrayList<String> cellValueName;
- 
-    private double latitude = 48.7433425;
-    private double longitude = 9.3201122;
-    
-    public static void main(String[] args) {
+	private TableView<BasicAircraft> table = new TableView<BasicAircraft>();
+	private ObservableList<BasicAircraft> aircraftList = FXCollections.observableArrayList();
+	private ArrayList<String> fields;
+	private ArrayList<String> cellValueName;
+
+	private double latitude = 48.7433425;
+	private double longitude = 9.3201122;
+
+	public static void main(String[] args) {
 		launch(args);
-    }
- 
-    @Override
-    public void start(Stage stage) {
- 		String urlString;
+	}
+
+	@Override
+	public void start(Stage stage) {
+		String urlString;
 		urlString = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json";
-		
+
 		PlaneDataServer server = new PlaneDataServer(urlString, latitude, longitude, 50);
 		new Thread(server).start();
 
 		Senser senser = new Senser(server);
 		new Thread(senser).start();
-		
+
 		Messer messer = new Messer();
 		senser.addObserver(messer);
 		new Thread(messer).start();
-		
+
 		// TODO: create activeAircrafts
 		activeAircrafts = new ActiveAircrafts();
-		
-		// TODO: activeAircrafts and Acamo needs to observe messer  
+
+		// TODO: activeAircrafts and Acamo needs to observe messer
 		messer.addObserver(this);
 		messer.addObserver(activeAircrafts);
-        fields = BasicAircraft.getAttributesNames();
-        cellValueName = BasicAircraft.getAttributesValues();
+		fields = BasicAircraft.getAttributesNames();
+		cellValueName = BasicAircraft.getAttributesValues();
 
-        // TODO: Fill column header using the attribute names from BasicAircraft
-		for(int i = 0;i < fields.size();i++) {
+		// TODO: Fill column header using the attribute names from BasicAircraft
+		for (int i = 0; i < fields.size(); i++) {
 			TableColumn<BasicAircraft, String> col = new TableColumn<BasicAircraft, String>(fields.get(i));
 			col.setCellValueFactory(new PropertyValueFactory(cellValueName.get(i)));
 			table.getColumns().add(col);
@@ -75,29 +73,36 @@ public class Acamo extends Application implements Observer
 		table.setItems(aircraftList);
 
 		table.setEditable(false);
-        table.autosize();
- 
-        // TODO: Create layout of table and pane for selected aircraft
-        Pane rightPane = new Pane();
-        VBox left = new VBox(new Label("Active Aircrafts"), table);
-        HBox main = new HBox(left, rightPane);
-        
-        // TODO: Add event handler for selected aircraft
- 
-        
-		Scene scene = new Scene(main);
-        stage.setScene(scene);
-        stage.setTitle("Acamo");
-        stage.sizeToScene();
-        stage.setOnCloseRequest(e -> Platform.exit());
-        stage.show();
-    }
+		table.autosize();
 
-    
-    // TODO: When messer updates Acamo (and activeAircrafts) the aircraftList must be updated as well
-    @Override
-    public void update(Observable o, Object arg) {
-    	aircraftList.clear();
-    	aircraftList.addAll(activeAircrafts.values());
-    }
+		// TODO: Create layout of table and pane for selected aircraft
+		Pane rightPane = new Pane();
+		VBox left = new VBox(new Label("Active Aircrafts"), table);
+		HBox main = new HBox(left, rightPane);
+
+		// TODO: Add event handler for selected aircraft
+		table.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if (event.isPrimaryButtonDown() && event.getClickCount() == 2){
+					System.out.println(table.getSelectionModel().getSelectedItem());
+				}
+			}
+		});
+
+		Scene scene = new Scene(main);
+		stage.setScene(scene);
+		stage.setTitle("Acamo");
+		stage.sizeToScene();
+		stage.setOnCloseRequest(e -> Platform.exit());
+		stage.show();
+	}
+
+	// TODO: When messer updates Acamo (and activeAircrafts) the aircraftList
+	// must be updated as well
+	@Override
+	public void update(Observable o, Object arg) {
+		aircraftList.clear();
+		aircraftList.addAll(activeAircrafts.values());
+	}
 }
